c2d57ab40df7ff8ef1acd1371aa1d4e6ee18a364
MKhairulHamid <mkhairulhamid@gmail.comn>
2020-02-28 15:05:06 +0700

    install and use library needed

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express')
-const cors = require('cors')
 const bodyParser = require('body-parser')
-
-const PORT = 4000
+const cors = require('cors')
+const bearerToken = require('express-bearer-token')
 
 const app = express()
+const PORT = 4000
 
-app.use(cors())
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(cors())
+app.use(express.static('public'))
+app.use(bearerToken())
+
 
 app.get('/', (req,res) =>{
     res.status(202).send('<h1> Welcome to ALevel.id API ! </h1>')
