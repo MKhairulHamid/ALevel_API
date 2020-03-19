73b02a10bfcff78984f24d69573ae2d6115a9489
MKhairulHamid <mkhairulhamid@gmail.comn>
2020-03-19 22:59:18 +0700

    add patch and delete

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -22,5 +22,35 @@ module.exports = {
             }
             res.status(200).send(results)
         })
+    },
+    addCourse : (req,res) => {
+
+        var sql = `INSERT INTO courses SET? `
+
+        sqlDB.query(sql, req.body, (err,results) => {
+            if(err) return res.status(500).send({message: 'Database Error', err, error: true})
+
+            res.status(200).send({ results: results})
+        })
+    },
+    editCourse : (req,res) => {
+
+        var sql = `UPDATE courses SET? WHERE id=${req.body.id}`
+
+        sqlDB.query(sql, req.body, (err, results) => {
+            if(err) return res.status(500).send({message:  'Database Error', err, error: true})
+
+            res.status(200).send({ results: results})
+        })
+
+    },
+    deleteCourse : (req,res) => {
+        var sql =  `DELETE FROM courses WHERE id=${req.params.id}`
+
+        sqlDB.query(sql, (err,results) => {
+            if(err) return res.status(500).send({message:  'Database Error', err, error: true})
+
+            res.status(200).send({ results: results})
+        })
     }
 }
--- routers/courseRouter.js
+++ routers/courseRouter.js
@@ -2,10 +2,14 @@ const express = require('express')
 const { auth } = require('../helpers/auth')
 const { courseController } = require('../controllers')
 
+
 const router = express.Router()
 
 router.get('/getall', courseController.getAllCourse)
 router.get('/getbyid/:id', courseController.getAllCourseById)
+router.post('/addcourse', auth,  courseController.addCourse)
+router.patch('/editcourse', auth, courseController.editCourse)
+router.delete('/deletecourse/:id',  courseController.deleteCourse)
 
 
 module.exports = router
